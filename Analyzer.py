5956e9e03e5bf25e0678ffd37685cc5c449fe59a
kkarnik <kkarnik@caltech.edu>
2017-07-13 14:25:59 -0400

    Added import and reformatting capability from GenomAD browser

--- Analyzer.py
+++ Analyzer.py
@@ -112,6 +112,34 @@ class Application(Frame):
         self.agenerator()
         self.zeroscreator()
 
+        # Import data from GenomAD Browser
+        source = open('genomeADalts.txt', 'r')
+        target = open('reformatgenomeAD.txt', 'w')
+
+        for line in source.readlines():
+            if(line == 'A\n'):
+                target.write('1\n')
+            elif(line == 'G\n'):
+                target.write('2\n')
+            elif(line == 'C\n'):
+                target.write('3\n')
+            elif(line == 'T\n'):
+                target.write('4\n')
+            else:
+                for char in line:
+                    if(char == 'A'):
+                        target.write('1')
+                    elif(char == 'G'):
+                        target.write('2')
+                    elif(char == 'C'):
+                        target.write('3')
+                    elif(char == 'T'):
+                        target.write('4')
+                target.write('\n')
+
+        source.close()
+        target.close()
+
         print("Python analysis complete. mergez.txt file and namelist.txt file generated.\n")
 
     def isvalidregion(self, geneloc):
